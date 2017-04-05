8b47454e4fb4e94a78d04dcb9f6dac5e89680730
Mike Brown <MBrown@1800contacts.com>
2017-04-05 15:09:30 -0600

    finishing the next lesson

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
+})
+    
+export class HeroesComponent  {
+    constructor(private heroService: HeroService, private router: Router) { }
+    title = 'Tour of Heroes';
+    selectedHero: Hero;    
+    heroes: Hero[];
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+}
